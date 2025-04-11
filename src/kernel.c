4fc1242137307a7d4498d9724216272776289fc3
Hoppy <dev.hoppy@gmail.com>
2025-04-11 14:13:29 +0200

    Fix line prompting

--- src/kernel.c
+++ src/kernel.c
@@ -27,28 +27,22 @@ typedef struct
 screen_t screens[MAX_SCREEN] = {0};
 uint8_t current_screen = 0;
 
-void reset_prompt()
-{
-	vga_clear_line(VGA_HEIGHT - 1);
-	printf(VGA_HEIGHT - 1, PROMPT "%s ", screens[current_screen].line);
-	vga_set_cursor(VGA_HEIGHT - 1, PROMPT_LEN + screens[current_screen].cursor_x);
-}
-
 static inline void cpuid(uint32_t code, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d)
 {
-       asm volatile("cpuid" : "=a"(*a), "=b"(*b), "=c"(*c), "=d"(*d) : "a"(code));
+	asm volatile("cpuid" : "=a"(*a), "=b"(*b), "=c"(*c), "=d"(*d) : "a"(code));
 }
 
 void display_cpu_info()
 {
-	char line [VGA_WIDTH];
+	char line[VGA_WIDTH];
 	int line_cursor = 0;
 	ft_memset(line, '\0', VGA_WIDTH);
 
-    uint32_t eax, ebx, ecx, edx;
-    cpuid(0, &eax, &ebx, &ecx, &edx);
-	
-	for(uint8_t i = 0; i <= 24; i += 8){
+	uint32_t eax, ebx, ecx, edx;
+	cpuid(0, &eax, &ebx, &ecx, &edx);
+
+	for (uint8_t i = 0; i <= 24; i += 8)
+	{
 		line[line_cursor] = (unsigned char)(ebx >> i);
 		line[line_cursor + 4] = (unsigned char)(edx >> i);
 		line[line_cursor + 8] = (unsigned char)(ecx >> i);
@@ -56,7 +50,6 @@ void display_cpu_info()
 	}
 
 	add_line_to_history(line, ft_strlen(line));
-	reset_prompt();
 }
 
 void draw_screen()
@@ -83,20 +76,27 @@ void draw_screen()
 	}
 }
 
+void draw_prompt()
+{
+	vga_clear_line(VGA_HEIGHT - 1);
+	printf(VGA_HEIGHT - 1, PROMPT "%s ", screens[current_screen].line);
+	vga_set_cursor(VGA_HEIGHT - 1, PROMPT_LEN + screens[current_screen].cursor_x);
+}
 
-void add_line_to_history(char *line, uint32_t len) {
+void add_line_to_history(char *line, uint32_t len)
+{
 	screen_t *screen = &screens[current_screen];
 	ft_memset(screen->data[screen->last_line], 0, VGA_WIDTH);
 	ft_memcpy(screen->data[screen->last_line], line, MIN(len, VGA_WIDTH));
 	ft_memset(line, 0, MIN(len, VGA_WIDTH));
 	screen->cursor_x = 0;
-	screen->len = 0;
 	screen->scroll = 0;
 	screen->last_line = (screen->last_line + 1) % (SCREEN_HEIGHT);
 	draw_screen();
 }
 
-void prompt_handling(uint8_t *key, uint8_t *last_key) {
+void prompt_handling(uint8_t *key, uint8_t *last_key)
+{
 	*key = keyboard_read();
 	uint8_t scancode = intb(KEYBOARD_DATA_PORT);
 	screen_t *screen = &screens[current_screen];
@@ -120,7 +120,7 @@ void prompt_handling(uint8_t *key, uint8_t *last_key) {
 			}
 			screen->len++;
 			screen->cursor_x++;
-			reset_prompt();
+			draw_prompt();
 		}
 		switch (*key)
 		{
@@ -145,11 +145,15 @@ void prompt_handling(uint8_t *key, uint8_t *last_key) {
 		case '\n':
 			if (screen->len > 0)
 			{
-				if (ft_strncmp(screen->line, "info cpu", 8) == 0) {
+				char line_cpy[MAX_LINE + 1] = {0};
+				ft_memcpy(line_cpy, screen->line, MAX_LINE);
+				add_line_to_history(screen->line, screen->len);
+				screen->len = 0;
+				draw_prompt();
+				if (ft_strcmp(line_cpy, "info cpu") == 0)
+				{
 					display_cpu_info();
 				}
-				add_line_to_history(screen->line, screen->len);
-				reset_prompt();
 			}
 			break;
 		case '\b':
@@ -158,12 +162,12 @@ void prompt_handling(uint8_t *key, uint8_t *last_key) {
 			screen->cursor_x--;
 			ft_memmove(screen->line + screen->cursor_x, screen->line + screen->cursor_x + 1, screen->len - screen->cursor_x);
 			screen->len--;
-			reset_prompt();
+			draw_prompt();
 			break;
 		case 132: // Page up
 			current_screen = (current_screen + 1) % MAX_SCREEN;
 			draw_screen();
-			reset_prompt();
+			draw_prompt();
 			break;
 		case 133: // Page down
 			if (current_screen == 0)
@@ -171,7 +175,7 @@ void prompt_handling(uint8_t *key, uint8_t *last_key) {
 			else
 				current_screen--;
 			draw_screen();
-			reset_prompt();
+			draw_prompt();
 			break;
 		default:
 			break;
@@ -179,8 +183,6 @@ void prompt_handling(uint8_t *key, uint8_t *last_key) {
 		*last_key = *key;
 	}
 }
-
-
 
 /* Entry point */
 void kmain()
@@ -191,11 +193,8 @@ void kmain()
 	uint8_t key = 0;
 
 	printf(0, "42\n");
-	reset_prompt();
-	// display_cpu_info();
+	draw_prompt();
 
-	// printf("")
-	reset_prompt();
 	while (42)
 	{
 		prompt_handling(&key, &last_key);
